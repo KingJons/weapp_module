4fef3d9ee4d7cf1dd50c9ccc14c735ac22ce29ed
shunwuyu <shunwu2001@163.com>
2017-11-23 11:12:16 +0800

    star over

--- pages/star/star.js
+++ pages/star/star.js
@@ -5,9 +5,80 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    stars: [0,1,2,3,4],
+    normalSrc: '../../images/normal.png',
+    selectedSrc: '../../images/selected.png',
+    halfSrc: '../../images/half.png',
+    key: 1,
+    path: '',
+    userInput: ''
   },
-
+  selectLeft: function(e) {
+    var key = e.currentTarget.dataset.key;
+    if (this.data.key == 0.5 && e.currentTarget.dataset.key == 0.5) {
+      key=0;
+    }
+    console.log("得"+key+"分");
+    this.setData({
+      key: key
+    });
+  },
+  selectRight: function(e) {
+    var key = e.currentTarget.dataset.key;
+    console.log("得" + key + "分");
+    this.setData({
+      key: key
+    })
+  },
+  upload: function() {
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: (res) => {
+        var tempFilePaths = res.tempFilePaths
+        console.log(tempFilePaths);
+        wx.uploadFile({
+          url: 'http://example.weixin.qq.com/upload',
+          filePath: tempFilePaths[0],
+          name: 'file',
+          formData: {
+            'user': 'test'
+          },
+          success: (res) => {
+            var data = res.data;
+            wx.showModal({
+              title: '上传文件返回状态',
+              content: '成功',
+              success: (res) => {
+                if (res.confirm) {
+                  console.log('用户点击确定');
+                }
+              }
+            })
+          }
+        })
+        this.setData({
+          path: tempFilePaths
+        })
+      }
+    })
+  },
+  textAreaCon: function(e) {
+    this.setData({
+      userInput: e.detail.value,
+    })
+  },
+  saveData: function() {
+    wx.showModal({
+      title: '用户填写数据',
+      content: `
+      评分：${this.data.key}
+      内容：${this.data.userInput}
+      文件：${this.data.path.join(',')}
+      `
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
